15611bd37cd3fe8c7c1fdff8b3a6a65feead4fe2
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-25 06:13:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts (" #{bard[0]} | #{bard[1]} | #{bard[2]} ")
+  puts (" #{board[0]} | #{board[1]} | #{board[2]} ")
   puts ("-----------")
-  puts (" #{bard[3]} | #{bard[4]} | #{bard[5]} ")
+  puts (" #{board[3]} | #{board[4]} | #{board[5]} ")
   puts ("-----------")
-  puts (" #{bard[6]} | #{bard[7]} | #{bard[8]} ")
+  puts (" #{board[6]} | #{board[7]} | #{board[8]} ")
 end
